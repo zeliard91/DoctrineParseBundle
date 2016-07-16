ad10e19694d3484d5fec1c167e2dfb12266508aa
Damien Matabon <damien.matabon@gmail.com>
2016-07-16 03:27:03 -0400

    Fix FOS LoginManager bridge
    Reset a null du password apres update pour que le user en session soit comme celui de la DB (sinon loop redirect apres password reset)

--- Bridge/FOSUser/UserManager.php
+++ Bridge/FOSUser/UserManager.php
@@ -43,4 +43,18 @@ class UserManager extends BaseUserManager
             $user->eraseCredentials();
         }
     }
+
+    /**
+     * Clear the password after the update so that the object be the same as when it is fetched
+     * (fix bug in reset password process where token->hasUserChanged() returns true after success reset -> redirect loop)
+     *
+     * @param UserInterface $user
+     * @param Boolean       $andFlush Whether to flush the changes (default true)
+     */
+    public function updateUser(UserInterface $user, $andFlush = true)
+    {
+        parent::updateUser($user, $andFlush);
+
+        $user->setPassword(null);
+    }
 }
