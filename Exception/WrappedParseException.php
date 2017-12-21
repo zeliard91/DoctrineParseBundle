0237334fdefdf2f0ea7162c3a2fa25b1bd4d53c1
Damien Matabon <damien.matabon@gmail.com>
2017-12-21 01:50:39 +0100

    Display batch errors in wrappedexection

--- Exception/WrappedParseException.php
+++ Exception/WrappedParseException.php
@@ -3,6 +3,7 @@
 namespace Redking\ParseBundle\Exception;
 
 use Symfony\Component\HttpKernel\Exception\HttpException;
+use Parse\ParseAggregateException;
 use Parse\ParseException;
 
 /**
@@ -12,6 +13,15 @@ class WrappedParseException extends HttpException
 {
     public function __construct(ParseException $previous)
     {
-        parent::__construct(502, $previous->getMessage(), $previous);
+        $message = $previous->getMessage();
+
+        if ($previous instanceof ParseAggregateException) {
+            foreach ($previous->getErrors() as $error) {
+                $message .= ' [' . $error['code'] . '] ' . $error['object']->getClassname() . 
+                    '(' . $error['object']->get('objectId') . ') : ' . $error['error'] . ' , ';
+            }
+        }
+
+        parent::__construct(502, $message, $previous);
     }
 }
