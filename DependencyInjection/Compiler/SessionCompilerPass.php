12a830c0c69759c59591dbf210d8a7a54621d4c4
Damien Matabon <damien.matabon@gmail.com>
2022-03-07 10:49:42 +0100

    fix session injection for sf4

--- DependencyInjection/Compiler/SessionCompilerPass.php
+++ DependencyInjection/Compiler/SessionCompilerPass.php
@@ -2,6 +2,7 @@
 
 namespace Redking\ParseBundle\DependencyInjection\Compiler;
 
+use InvalidArgumentException;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 
@@ -13,7 +14,11 @@ class SessionCompilerPass implements CompilerPassInterface
     public function process(ContainerBuilder $container): void
     {
         $parseSessionDef = $container->getDefinition('doctrine_parse.session_storage');
-        $sessionDef = $container->getAlias('session');
+        try {
+            $sessionDef = $container->getAlias('session');
+        } catch (InvalidArgumentException $e) {
+            $sessionDef = $container->getDefinition('session');
+        }
 
         if (!$sessionDef->isDeprecated()) {
             $parseSessionDef->addMethodCall('setSession', [$sessionDef]);
