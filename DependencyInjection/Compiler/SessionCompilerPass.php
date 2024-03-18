36dc245cf30ae178c6c5a668ea61ab7ba4a6e238
Damien Matabon <damien.matabon@gmail.com>
2024-03-18 16:10:04 +0100

    handle cases where session is not a service

--- DependencyInjection/Compiler/SessionCompilerPass.php
+++ DependencyInjection/Compiler/SessionCompilerPass.php
@@ -5,6 +5,7 @@ namespace Redking\ParseBundle\DependencyInjection\Compiler;
 use InvalidArgumentException;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\Exception\ServiceNotFoundException;
 
 /**
  * Inject session or request stack 
@@ -14,13 +15,16 @@ class SessionCompilerPass implements CompilerPassInterface
     public function process(ContainerBuilder $container): void
     {
         $parseSessionDef = $container->getDefinition('doctrine_parse.session_storage');
+        $sessionDef = null;
         try {
             $sessionDef = $container->getAlias('session');
         } catch (InvalidArgumentException $e) {
-            $sessionDef = $container->getDefinition('session');
+            try {
+                $sessionDef = $container->getDefinition('session');
+            } catch (ServiceNotFoundException $th) {}
         }
 
-        if (!$sessionDef->isDeprecated()) {
+        if (null !== $sessionDef && !$sessionDef->isDeprecated()) {
             $parseSessionDef->addMethodCall('setSession', [$sessionDef]);
         }
     }
